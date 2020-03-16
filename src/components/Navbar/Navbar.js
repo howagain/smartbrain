3159747cfa33b1b238c4f0a560faa8f94386e8be
Jacob Haugen <howagain@gmail.com>
2020-03-16 10:25:15 -0400

    added navbar and component folders

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav style={{textAlign: 'right'}}>
+      <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
+    </nav>
+  )
+}
